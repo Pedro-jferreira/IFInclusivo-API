dd168437846e639fdca3de8d50c07ae2649f2a2c
Pedro Ferreira <pedrojferreiradev@gmail.com>
2024-07-05 09:22:32 -0300

    first commit

---
+++ src/main/java/com/example/IfGoiano/IfCoders/IfCodersApplication.java
@@ -0,0 +1,13 @@
+package com.example.IfGoiano.IfCoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfCodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfCodersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IfGoiano/IfCoders/IfCodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IfGoiano.IfCoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfCodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

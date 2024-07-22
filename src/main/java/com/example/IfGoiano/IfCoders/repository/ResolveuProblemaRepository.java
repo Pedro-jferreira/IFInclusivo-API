c353f0563fb918c46c080222f929a756d3bd9555
Pedro Ferreira <pedrojferreiradev@gmail.com>
2024-07-22 04:26:09 -0300

    interface ResolveuProblemaRepository.java alterada para se adptar as novas mudancas

--- src/main/java/com/example/IfGoiano/IfCoders/repository/ResolveuProblemaRepository.java
+++ src/main/java/com/example/IfGoiano/IfCoders/repository/ResolveuProblemaRepository.java
@@ -1,7 +1,8 @@
 package com.example.IfGoiano.IfCoders.repository;
 
+import com.example.IfGoiano.IfCoders.model.PK.ResolveuProblemaId;
 import com.example.IfGoiano.IfCoders.model.ResolveuProblema;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResolveuProblemaRepository extends JpaRepository<ResolveuProblema, Long> {
+public interface ResolveuProblemaRepository extends JpaRepository<ResolveuProblema, ResolveuProblemaId> {
 }

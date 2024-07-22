adca6bed5ef2ace4c189050ef0357c556c9af1c8
Pedro Ferreira <pedrojferreiradev@gmail.com>
2024-07-22 04:21:53 -0300

    Class ComentarioService.java criada e previamente configurada

--- src/main/java/com/example/IfGoiano/IfCoders/service/ComentarioService.java
+++ src/main/java/com/example/IfGoiano/IfCoders/service/ComentarioService.java
@@ -2,79 +2,126 @@ package com.example.IfGoiano.IfCoders.service;
 
 import com.example.IfGoiano.IfCoders.model.Comentario;
 import com.example.IfGoiano.IfCoders.model.PK.ComentarioId;
+import com.example.IfGoiano.IfCoders.model.Publicacao;
+import com.example.IfGoiano.IfCoders.model.Usuario;
 import com.example.IfGoiano.IfCoders.repository.ComentarioRepository;
 import com.example.IfGoiano.IfCoders.service.Exception.DataBaseException;
 import com.example.IfGoiano.IfCoders.service.Exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ComentarioService {
 
     @Autowired
-    private ComentarioRepository repository;
+    private ComentarioRepository comentarioRepository;
 
     public List<Comentario> findAll() {
-        return repository.findAll();
+        try {
+            return comentarioRepository.findAll();
+        } catch (DataAccessException e) {
+            throw new DataBaseException("Database error occurred while fetching all comments"+ e);
+        }
     }
 
     public Comentario findById(ComentarioId id) {
-        return (Comentario) repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+        try {
+            Optional<Comentario> comentario = comentarioRepository.findById(id);
+            return comentario.orElseThrow(() -> new ResourceNotFoundException(id));
+        } catch (DataAccessException e) {
+            throw new DataBaseException("Database error occurred while fetching comment : " + e);
+        }
     }
 
+    @Transactional
     public Comentario save(Comentario comentario) {
         try {
-            return repository.save(comentario);
-        } catch (Exception e) {
-            throw new DataBaseException("Error saving Comentario: " + e.getMessage());
+            if (comentario.getComentarioPai() == null) {
+                comentario.setComentarioPai(comentario);
+            }
+            return comentarioRepository.save(comentario);
+        } catch (DataAccessException e) {
+            throw new DataBaseException("Database error occurred while saving the comment"+ e);
         }
     }
 
-    public Comentario update(Comentario comentario) {
-        if (!repository.existsById(comentario.getId())) {
-            throw new ResourceNotFoundException(comentario.getId());
-        }
+    @Transactional
+    public Comentario update(ComentarioId id, Comentario comentarioDetails) {
         try {
-            return repository.save(comentario);
-        } catch (Exception e) {
-            throw new DataBaseException("Error updating Comentario: " + e.getMessage());
+            Comentario comentario = findById(id);
+            updateComentarioDetails(comentario, comentarioDetails);
+            return comentarioRepository.save(comentario);
+        } catch (DataAccessException e) {
+            throw new DataBaseException("Database error occurred while updating the comment"+ e);
         }
     }
 
-    public void deleteById(ComentarioId id) {
+    @Transactional
+    public void delete(ComentarioId id) {
         try {
-            repository.deleteById(id);
-        } catch (EmptyResultDataAccessException e) {
-            throw new ResourceNotFoundException(id);
-        } catch (Exception e) {
-            throw new DataBaseException("Error deleting Comentario: " + e.getMessage());
+            Comentario comentario = findById(id);
+            comentarioRepository.delete(comentario);
+        } catch (DataAccessException e) {
+            throw new DataBaseException("Database error occurred while deleting the comment"+ e);
         }
     }
-
-    public List<ComentarioRepository> findByComentarioPai(Comentario comentarioPai) {
-        return repository.findByComentarioPai(comentarioPai);
+    public Optional<Comentario> findByUsuarioAndPublicacao(Usuario usuario, Publicacao publicacao){
+        ComentarioId comentarioId = new ComentarioId();
+        comentarioId.setUsuario(usuario);
+        comentarioId.setPublicacao(publicacao);
+        return comentarioRepository.findById(comentarioId);
     }
+    @Transactional
+    public Comentario receberSubComentario(ComentarioId comentarioPaiId, Usuario usuario, Comentario comentario){
 
-    public Comentario addSubComentario(Comentario comentarioPai, Comentario subComentario) {
-        subComentario.setComentarioPai(comentarioPai);
+      try {
+          Optional<Comentario> comentarioOptional = comentarioRepository.findById(comentarioPaiId);
+          if (comentarioOptional.isPresent()){
+              Comentario comentarioPai = comentarioOptional.get();
+              comentario.setPublicacao(comentarioPai.getPublicacao());
+              comentarioPai.addChildCommentToComment(comentario);
+              usuario.addCommentToUser(comentario);
+              comentarioRepository.save(comentario);
+              comentarioRepository.save(comentarioPai);
+              return comentario;
+          }else throw  new ResourceNotFoundException(comentarioPaiId);
+      } catch (DataBaseException e){
+          throw new DataBaseException("Database error occurred when adding a Comment to the comment" + e);
+      }
+
+    }
+    @Transactional
+    public void removerSubComentario(ComentarioId comentarioPaiId, ComentarioId subComentarioId) {
         try {
-            return repository.save(subComentario);
-        } catch (Exception e) {
-            throw new DataBaseException("Error adding subComentario: " + e.getMessage());
+            Optional<Comentario> comentarioPaiOptional = comentarioRepository.findById(comentarioPaiId);
+            if (comentarioPaiOptional.isPresent()) {
+                Comentario comentarioPai = comentarioPaiOptional.get();
+                Optional<Comentario> subComentarioOptional = comentarioRepository.findById(subComentarioId);
+                if (subComentarioOptional.isPresent()) {
+                    Comentario subComentario = subComentarioOptional.get();
+                    comentarioPai.removeChildCommentFromComment(subComentario);
+                    comentarioRepository.save(comentarioPai);
+                    comentarioRepository.delete(subComentario);
+                } else {
+                    throw new ResourceNotFoundException(subComentarioId);
+                }
+            } else {
+                throw new ResourceNotFoundException(comentarioPaiId);
+            }
+        } catch (DataBaseException e) {
+            throw new DataBaseException("Database error occurred when removing a sub-comment" + e);
         }
     }
-
-    public void removeSubComentario(Comentario comentarioPai, ComentarioId subComentarioId) {
-        Comentario subComentario = (Comentario) repository.findById(subComentarioId)
-                .orElseThrow(() -> new ResourceNotFoundException(subComentarioId));
-        comentarioPai.getComentariosFilhos().remove(subComentario);
-        try {
-            repository.save(comentarioPai);
-        } catch (Exception e) {
-            throw new DataBaseException("Error removing subComentario: " + e.getMessage());
-        }
+    private void updateComentarioDetails(Comentario comentario, Comentario comentarioDetails) {
+        comentario.setContent(comentarioDetails.getContent());
+        comentario.setLocalDateTime(comentarioDetails.getLocalDateTime());
+        comentario.setComentarioPai(comentarioDetails.getComentarioPai());
+        comentario.setResolveuProblemas(comentarioDetails.getResolveuProblemas());
+        comentario.setComentariosFilhos(comentarioDetails.getComentariosFilhos());
     }
 }

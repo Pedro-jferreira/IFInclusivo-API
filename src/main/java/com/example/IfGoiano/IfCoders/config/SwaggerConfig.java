d0036aeff68f68118a2855ce9a2c6a45695e8b7a
Pedro Ferreira <pedrojferreiradev@gmail.com>
2024-07-22 15:00:51 -0300

    Class SwaggerConfig.java updated

--- src/main/java/com/example/IfGoiano/IfCoders/config/SwaggerConfig.java
+++ src/main/java/com/example/IfGoiano/IfCoders/config/SwaggerConfig.java
@@ -16,8 +16,16 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API do seu Projeto")
-                        .description("Descrição da sua API")
+                        .title("IF-Inclusivo api")
+                        .description("Esta API faz parte de um projeto de extensão desenvolvido" +
+                                " na disciplina Projeto Integrador. O objetivo da disciplina é " +
+                                "reunir três professores e grupos de alunos para integrar o conhecimento" +
+                                " de várias matérias e, ao final, entregar um projeto de extensão. Neste" +
+                                " caso, o projeto é um aplicativo destinado a ajudar pessoas com necessidades" +
+                                " específicas. O aplicativo é um fórum onde essas pessoas podem enviar suas" +
+                                " dúvidas e receber ajuda de professores e alunos. Além disso, o aplicativo" +
+                                " conta com um glossário de Libras, com palavras específicas para programação," +
+                                " gerenciado por tutores que adicionam palavras e vídeos de tradução para Libras.")
                         .version("1.0.0")
                 )
                 .servers(Collections.singletonList(new Server().url("http://localhost:8080")));

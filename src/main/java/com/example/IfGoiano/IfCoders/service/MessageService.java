274d1121f307efc989ac8fe109bcc954f4bb6c09
Pedro Ferreira <pedrojferreiradev@gmail.com>
2024-08-20 09:46:47 -0300

    update

--- src/main/java/com/example/IfGoiano/IfCoders/service/MessageService.java
+++ src/main/java/com/example/IfGoiano/IfCoders/service/MessageService.java
@@ -4,32 +4,21 @@ import com.example.IfGoiano.IfCoders.model.MessageEntity;
 import com.example.IfGoiano.IfCoders.repository.MessageRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class MessageService {
     @Autowired
     private MessageRepository messageRepository;
-    public MessageEntity createMessage(MessageEntity entity) {
-        return messageRepository.save(entity);
-    }
-    public List<MessageEntity> findAllMessagem() {
-        return messageRepository.findAll();
-    }
-    public MessageEntity getById(Long id) {
-        return messageRepository.getReferenceById(id);
-    }
-    public List<MessageEntity> getExchangedMessages(Long userEnvia, Long userRecebe){
-        return messageRepository.getExchangedMessages(userEnvia,userRecebe);
-    }
-    public void deleteById(Long id) {
-        messageRepository.deleteById(id);
-    }
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
 
 
+    public void sendMessage(Long sender, Long receiver, String text) {
+        // Cria uma nova mensagem
 
+    }
 
 }

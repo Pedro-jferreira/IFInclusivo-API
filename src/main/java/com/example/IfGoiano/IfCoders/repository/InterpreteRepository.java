e08abd7dddb3d1d6729b0f97551fe1cb6c3b51ab
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-23 16:00:22 -0300

    :recycle: refactor: Mapera a entity renomeada Interprete

--- src/main/java/com/example/IfGoiano/IfCoders/repository/InterpreteRepository.java
+++ src/main/java/com/example/IfGoiano/IfCoders/repository/InterpreteRepository.java
@@ -1,10 +1,10 @@
 package com.example.IfGoiano.IfCoders.repository;
 
-import com.example.IfGoiano.IfCoders.model.InterpreteEntity;
+import com.example.IfGoiano.IfCoders.model.Interprete;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InterpreteRepository extends JpaRepository<InterpreteEntity, Long> {
+public interface InterpreteRepository extends JpaRepository<Interprete, Long> {
 
 }

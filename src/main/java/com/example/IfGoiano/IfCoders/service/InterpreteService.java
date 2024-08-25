b44786c6a4f32a57d4e050812c52758b3403bac6
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-25 10:45:34 -0300

    :sparkles: criar interface do Interprete

--- src/main/java/com/example/IfGoiano/IfCoders/service/InterpreteService.java
+++ src/main/java/com/example/IfGoiano/IfCoders/service/InterpreteService.java
@@ -1,2 +1,18 @@
-package com.example.IfGoiano.IfCoders.service;public interface InterpreteService {
+package com.example.IfGoiano.IfCoders.service;
+
+import com.example.IfGoiano.IfCoders.controller.DTO.input.InterpreteInputDTO;
+import com.example.IfGoiano.IfCoders.controller.DTO.output.InterpreteOutputDTO;
+
+import java.util.List;
+
+public interface InterpreteService {
+    List<InterpreteOutputDTO> findAll();
+
+    InterpreteOutputDTO findById(Long id);
+
+    InterpreteOutputDTO save(InterpreteInputDTO interpreteOutputDTO);
+
+    InterpreteOutputDTO update(InterpreteInputDTO interpreteOutputDTO, Long id);
+
+    void delete(Long id);
 }

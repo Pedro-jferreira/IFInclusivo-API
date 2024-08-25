f0e2e1258283354e95283a00e08e01458df5b671
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-25 13:44:44 -0300

    :sparkles: feat: criar mapper da classe tutor

---
+++ src/main/java/com/example/IfGoiano/IfCoders/controller/mapper/TutorMapper.java
@@ -0,0 +1,28 @@
+package com.example.IfGoiano.IfCoders.controller.mapper;
+
+import com.example.IfGoiano.IfCoders.controller.DTO.input.TutorInputDTO;
+import com.example.IfGoiano.IfCoders.controller.DTO.output.TutorOutputDTO;
+import com.example.IfGoiano.IfCoders.entity.TutorEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TutorMapper {
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    public TutorEntity toTutorEntity(TutorInputDTO tutorInput) {
+
+        return modelMapper.map(tutorInput, TutorEntity.class);
+    }
+
+    public TutorInputDTO toTutorInputDTO(TutorEntity tutorEntity) {
+        return modelMapper.map(tutorEntity, TutorInputDTO.class);
+    }
+
+    public TutorOutputDTO toTutorOutputDTO(TutorEntity tutorEntity) {
+        return modelMapper.map(tutorEntity, TutorOutputDTO.class);
+    }
+}

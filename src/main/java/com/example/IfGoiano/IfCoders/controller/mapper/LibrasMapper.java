94f7b8c57e6cc37c079a7769adc9fa6d71cb568b
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-25 13:45:00 -0300

    :sparkles: feat: criar mapper da classe libras

---
+++ src/main/java/com/example/IfGoiano/IfCoders/controller/mapper/LibrasMapper.java
@@ -0,0 +1,14 @@
+package com.example.IfGoiano.IfCoders.controller.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LibrasMapper {
+
+    @Autowired
+    ModelMapper modelMapper;
+
+
+}

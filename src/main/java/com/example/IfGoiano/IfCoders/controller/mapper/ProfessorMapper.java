f336ccb0ce2af7c35f3912cee0efd258e2151225
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-25 13:24:18 -0300

    implementa service professor

---
+++ src/main/java/com/example/IfGoiano/IfCoders/controller/mapper/ProfessorMapper.java
@@ -0,0 +1,2 @@
+package com.example.IfGoiano.IfCoders.controller.mapper;public class ProfessorMapper {
+}
---
+++ src/main/java/com/example/IfGoiano/IfCoders/service/ProfessorService.java
@@ -0,0 +1,2 @@
+package com.example.IfGoiano.IfCoders.service;public class ProfessorService {
+}

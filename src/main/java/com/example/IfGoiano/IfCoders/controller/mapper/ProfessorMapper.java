3f846d30d84a074a92ae31495eed93cddedf71ff
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-25 13:25:21 -0300

    feat: criar Mapper para classe professor

--- src/main/java/com/example/IfGoiano/IfCoders/controller/mapper/ProfessorMapper.java
+++ src/main/java/com/example/IfGoiano/IfCoders/controller/mapper/ProfessorMapper.java
@@ -1,2 +1,30 @@
-package com.example.IfGoiano.IfCoders.controller.mapper;public class ProfessorMapper {
+package com.example.IfGoiano.IfCoders.controller.mapper;
+
+import com.example.IfGoiano.IfCoders.controller.DTO.SimpleProfessorDTO;
+import com.example.IfGoiano.IfCoders.controller.DTO.input.ProfessorInputDTO;
+import com.example.IfGoiano.IfCoders.controller.DTO.output.ProfessorOutputDTO;
+import com.example.IfGoiano.IfCoders.entity.ProfessorEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProfessorMapper {
+    @Autowired
+    ModelMapper modelMapper;
+
+
+    public ProfessorEntity toProfessorEntity(ProfessorInputDTO professorInputDTO){
+        return modelMapper.map(professorInputDTO, ProfessorEntity.class);
+    }
+
+    public ProfessorOutputDTO toProfessorOutputDTO(ProfessorEntity professorEntity){
+        return modelMapper.map(professorEntity, ProfessorOutputDTO.class);
+    }
+
+    public SimpleProfessorDTO toSimpleProfessorDTO(ProfessorEntity professorEntity){
+        return modelMapper.map(professorEntity, SimpleProfessorDTO.class);
+    }
+
+
 }
--- src/main/java/com/example/IfGoiano/IfCoders/service/ProfessorService.java
+++ src/main/java/com/example/IfGoiano/IfCoders/service/ProfessorService.java
@@ -1,2 +1,18 @@
-package com.example.IfGoiano.IfCoders.service;public class ProfessorService {
+package com.example.IfGoiano.IfCoders.service;
+
+import com.example.IfGoiano.IfCoders.controller.DTO.input.ProfessorInputDTO;
+import com.example.IfGoiano.IfCoders.controller.DTO.output.ProfessorOutputDTO;
+
+import java.util.List;
+
+public interface ProfessorService {
+    List<ProfessorOutputDTO> findAll();
+
+    ProfessorOutputDTO findById(Long id);
+
+    ProfessorOutputDTO save(ProfessorInputDTO professorInputDTO);
+
+    ProfessorOutputDTO update(ProfessorInputDTO professorInputDTO, Long id);
+
+    void delete(Long id);
 }

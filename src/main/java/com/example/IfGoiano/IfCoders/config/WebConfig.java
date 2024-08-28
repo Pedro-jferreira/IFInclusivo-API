4f12feef65dcef490b8aa883f3c75456d041e725
Pedro Ferreira <pedrojferreiradev@gmail.com>
2024-08-28 08:01:04 -0300

    banco de dados travado

--- src/main/java/com/example/IfGoiano/IfCoders/config/WebConfig.java
+++ src/main/java/com/example/IfGoiano/IfCoders/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://ifinclusivo-api-production.up.railway.app", "https://outro-dominio-permitido.com")
+                .allowedOriginPatterns("https://ifinclusivo-api-production.up.railway.app", "https://ifcoders-api-production.up.railway.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

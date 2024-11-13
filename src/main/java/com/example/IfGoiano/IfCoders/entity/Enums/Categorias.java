525a1d6cdc20134c3a37fea328f3260c47702183
Pedro Ferreira <pedrojferreiradev@gmail.com>
2024-11-13 08:43:52 -0300

    feat(Categoria) classe enumerada Categoria [criacao da classe enumerada categoria para implementacao nas demais classes que precisarao dela.

---
+++ src/main/java/com/example/IfGoiano/IfCoders/entity/Enums/Categorias.java
@@ -0,0 +1,20 @@
+package com.example.IfGoiano.IfCoders.entity.Enums;
+
+public enum Categorias {
+    REDES("Redes"),
+    BANCO_DE_DADOS("Banco de Dados"),
+    PROGRAMACAO("Programação"),
+    WEB("Web"),
+    ESTRUTURA_DE_DADOS("Estrutura de Dados"),
+    ARQUITETURA_DE_COMPUTADORES("Arquitetura de Computadores");
+
+    private final String descricao;
+
+    Categorias(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}

5df08f552a0b0b5ddbc1c8ad4d2496e0ec9d1a6e
Pedro Ferreira <pedrojferreiradev@gmail.com>
2024-11-14 08:22:56 -0300

    fix(SimpleTopicoDTO): bug na troca de paremetro da classe enum Categorias [foi trocado o atributo de string para Enum Categorias]

--- src/main/java/com/example/IfGoiano/IfCoders/controller/DTO/SimpleTopicoDTO.java
+++ src/main/java/com/example/IfGoiano/IfCoders/controller/DTO/SimpleTopicoDTO.java
@@ -1,12 +1,13 @@
 package com.example.IfGoiano.IfCoders.controller.DTO;
 
+import com.example.IfGoiano.IfCoders.entity.Enums.Categorias;
 import lombok.Data;
 
 @Data
 public class SimpleTopicoDTO {
     private Long id;
     private String tema;
-    private String descripcion;
+    private Categorias descripcion;
     private String categoria;
     private SimpleProfessorDTO professor;
 }

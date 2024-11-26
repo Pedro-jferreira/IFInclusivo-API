1a4fe63d8cfb6e3a553eb0f8fa990b2e77a6aaae
Pedro Ferreira <pedrojferreiradev@gmail.com>
2024-11-26 09:20:03 -0300

    feat (Topicos) consultas relacionadas a topicos

--- src/main/java/com/example/IfGoiano/IfCoders/repository/LibrasRepository.java
+++ src/main/java/com/example/IfGoiano/IfCoders/repository/LibrasRepository.java
@@ -13,18 +13,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LibrasRepository extends JpaRepository<LibrasEntity, Long> {
-    Page<LibrasEntity> findByCategoria(Categorias categoria, Pageable pageable);
-
-    Page<LibrasEntity> findByPalavraContainingIgnoreCaseAndStatus(String termo, Status status, Pageable pageable);
-
-
-    @Query("SELECT l FROM LibrasEntity l " +
-            "WHERE (LOWER(l.palavra) LIKE LOWER(CONCAT('%', :termo, '%')) " +
-            "OR LOWER(l.descricao) LIKE LOWER(CONCAT('%', :termo, '%')) " +
-            "OR LOWER(l.categorias) LIKE LOWER(CONCAT('%', :termo, '%'))) " +
-            "AND l.status = :status")
-    Page<LibrasEntity> searchLibrasByTermDeeply(@Param("termo") String termo,
-                                                @Param("status") Status status,
-                                                Pageable pageable);
-
+    
 }

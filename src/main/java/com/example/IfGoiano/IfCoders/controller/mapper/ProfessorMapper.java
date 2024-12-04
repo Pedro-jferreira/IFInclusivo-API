27ab6e08e200823a129cdc6ae19ce97e3237c3bd
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2024-12-04 08:27:32 -0300

    :recycle: refactor(ProfessorMapper) toProfessorOutputDTO manual

--- src/main/java/com/example/IfGoiano/IfCoders/controller/mapper/ProfessorMapper.java
+++ src/main/java/com/example/IfGoiano/IfCoders/controller/mapper/ProfessorMapper.java
@@ -1,14 +1,20 @@
 package com.example.IfGoiano.IfCoders.controller.mapper;
 
 import com.example.IfGoiano.IfCoders.controller.DTO.SimpleProfessorDTO;
+import com.example.IfGoiano.IfCoders.controller.DTO.SimpleTopicoDTO;
 import com.example.IfGoiano.IfCoders.controller.DTO.input.ProfessorInputDTO;
 import com.example.IfGoiano.IfCoders.controller.DTO.input.UsuarioInputDTO;
+import com.example.IfGoiano.IfCoders.controller.DTO.output.ConfigAcblOutputDTO;
 import com.example.IfGoiano.IfCoders.controller.DTO.output.ProfessorOutputDTO;
 import com.example.IfGoiano.IfCoders.entity.ProfessorEntity;
+import com.example.IfGoiano.IfCoders.entity.TopicoEntity;
 import com.example.IfGoiano.IfCoders.entity.UsuarioEntity;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ProfessorMapper {
@@ -24,7 +30,42 @@ public class ProfessorMapper {
     }
 
     public ProfessorOutputDTO toProfessorOutputDTO(ProfessorEntity professorEntity){
-        return modelMapper.map(professorEntity, ProfessorOutputDTO.class);
+        ProfessorOutputDTO p = new ProfessorOutputDTO();
+        p.setId(professorEntity.getId());
+        p.setNome(professorEntity.getNome());
+        p.setBiografia(professorEntity.getBiografia());
+        p.setLogin(professorEntity.getLogin());
+        p.setSenha(professorEntity.getSenha());
+        p.setMatricula(professorEntity.getMatricula());
+        p.setDataCriacao(professorEntity.getDataCriacao());
+        p.setFormacao(professorEntity.getFormacao());
+        ConfigAcblOutputDTO a = new ConfigAcblOutputDTO();
+        a.setId(professorEntity.getConfigAcessibilidadeEntity().getId());
+        a.setTema(professorEntity.getConfigAcessibilidadeEntity().getTema());
+        a.setAudicao(professorEntity.getConfigAcessibilidadeEntity().getAudicao());
+        a.setZoom(professorEntity.getConfigAcessibilidadeEntity().getZoom());
+        p.setConfigAcessibilidadeEntity(a);
+
+        List<SimpleTopicoDTO> st = new ArrayList<>();
+        for (TopicoEntity t :professorEntity.getTopicos()) {
+            SimpleTopicoDTO aux = new SimpleTopicoDTO();
+            aux.setId(t.getId());
+            aux.setTitulo(t.getTitulo());
+            aux.setDescricao(t.getDescricao());
+            aux.setDataCriacao(t.getDataCriacao());
+            aux.setCategoria(t.getCategoria());
+            SimpleProfessorDTO sp = new SimpleProfessorDTO();
+            sp.setId(t.getProfessor().getId());
+            sp.setNome(t.getProfessor().getNome());
+            sp.setMatricula(t.getProfessor().getMatricula());
+            sp.setBiografia(t.getProfessor().getBiografia());
+            sp.setDataCriacao(t.getProfessor().getDataCriacao());
+            aux.setProfessor(sp);
+            st.add(aux);
+        }
+        p.setTopicos(st);
+
+        return p;
     }
 
     public SimpleProfessorDTO toSimpleProfessorDTO(ProfessorEntity professorEntity){

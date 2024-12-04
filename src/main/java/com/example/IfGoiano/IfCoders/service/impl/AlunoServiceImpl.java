d2e02ed20f4c6729c18974f91514bf652cba226a
Walmircsanto <walmirsantosrosa@gmail.com>
2024-12-04 07:42:41 -0300

    :recycle: refactor(AlunoServiceImpl): Adicionar regra de negocio no sugerir libras

--- src/main/java/com/example/IfGoiano/IfCoders/service/impl/AlunoServiceImpl.java
+++ src/main/java/com/example/IfGoiano/IfCoders/service/impl/AlunoServiceImpl.java
@@ -3,9 +3,12 @@ package com.example.IfGoiano.IfCoders.service.impl;
 import com.example.IfGoiano.IfCoders.controller.DTO.input.AlunoInputDTO;
 import com.example.IfGoiano.IfCoders.controller.DTO.output.AlunoOutputDTO;
 import com.example.IfGoiano.IfCoders.controller.mapper.AlunoMapper;
+import com.example.IfGoiano.IfCoders.entity.AlunoEntity;
 import com.example.IfGoiano.IfCoders.exception.ResourceNotFoundException;
 import com.example.IfGoiano.IfCoders.repository.AlunoRepository;
+import com.example.IfGoiano.IfCoders.repository.CursoRepository;
 import com.example.IfGoiano.IfCoders.service.AlunoService;
+import com.example.IfGoiano.IfCoders.service.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +23,8 @@ public class AlunoServiceImpl implements AlunoService {
     private AlunoRepository alunoRepository;
     @Autowired
     private AlunoMapper mapper;
+    @Autowired
+    private CursoRepository cursoRepository; // não sei se e a meelhor forma mas por enquanto vamos usar essa ideia
 
 
     @Override
@@ -36,14 +41,21 @@ public class AlunoServiceImpl implements AlunoService {
     @Override
     @Transactional
     public AlunoOutputDTO save(AlunoInputDTO aluno) {
-        return findById( alunoRepository.save(mapper.toAlunoEntity(aluno)).getId());
+        var curso = cursoRepository.findById(aluno.getCurso()).get();
+        if (curso == null) {
+            throw new ResourceNotFoundException("Curso not found curso id: " + aluno.getCurso());
+        }
+        AlunoEntity alunoEntity = mapper.toAlunoEntity(aluno);
+        alunoEntity.setCurso(curso);
+
+        return findById(alunoRepository.save(alunoEntity).getId());
     }
 
     @Override
     @Transactional
     public AlunoOutputDTO update(Long id, AlunoInputDTO alunoDetails) {
         var aluno = alunoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
-        mapper.updateAlunoEntityFromDTO( alunoDetails,aluno);
+        mapper.updateAlunoEntityFromDTO(alunoDetails, aluno);
         return mapper.toAlunoOutputDTO(alunoRepository.save(aluno));
     }
 

163b5ae647d6394fa1287c9cceb5398b1b660971
Walmircsanto <walmirsantosrosa@gmail.com>
2024-12-30 08:45:15 -0300

    :sparkles: feat(LibrasRepository): added new queries for Libras such as word search, deep search and shallow searches

--- src/main/java/com/example/IfGoiano/IfCoders/repository/LibrasRepository.java
+++ src/main/java/com/example/IfGoiano/IfCoders/repository/LibrasRepository.java
@@ -6,10 +6,26 @@ import com.example.IfGoiano.IfCoders.entity.LibrasEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LibrasRepository extends JpaRepository<LibrasEntity, Long> {
 
     Page<LibrasEntity> findAll(Pageable pageable);
+
+    Page<LibrasEntity> findByPalavra(String palavra, Pageable pageable);
+
+    Page<LibrasEntity> findByStatus(Status status, Pageable pageable);
+
+
+    @Query("SELECT DISTINCT l FROM LibrasEntity l " +
+            "WHERE LOWER(l.palavra) LIKE LOWER(CONCAT('%', :search, '%')) " +
+            "OR LOWER(l.descricao) LIKE LOWER(CONCAT('%',:search, '%')) ")
+    Page<LibrasEntity> searchLibrasByDeeply(@Param("search") String search, Pageable pageable);
+
+    LibrasEntity findByPalavra(String palavra);
+
+
 }

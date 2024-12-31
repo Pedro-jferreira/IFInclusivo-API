b07676f60dfcaea9b4f6d5ed907ca6c458efae4a
Walmircsanto <walmirsantosrosa@gmail.com>
2024-12-30 21:00:05 -0300

    :sparkles: feat(LibrasService): Adicionar metodos de busca profunda, busca rasa,e busca por status

--- src/main/java/com/example/IfGoiano/IfCoders/service/LibrasService.java
+++ src/main/java/com/example/IfGoiano/IfCoders/service/LibrasService.java
@@ -2,6 +2,9 @@ package com.example.IfGoiano.IfCoders.service;
 
 import com.example.IfGoiano.IfCoders.controller.DTO.input.LibrasInputDTO;
 import com.example.IfGoiano.IfCoders.controller.DTO.output.LibrasOutputDTO;
+import com.example.IfGoiano.IfCoders.entity.Enums.Status;
+import com.example.IfGoiano.IfCoders.entity.LibrasEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +23,14 @@ public interface LibrasService {
     LibrasOutputDTO sugereLibras(LibrasInputDTO librasInputDTO,Long id );
 
     LibrasOutputDTO update(LibrasInputDTO librasInputDTO ,Long id);
+
+    Page<LibrasOutputDTO> findByPalavra(String palavra, int pag, int itens);
+
+    Page<LibrasOutputDTO> findByStatus(Status status, int pag, int itens);
+
+    Page<LibrasOutputDTO> searchLibrasByDeeply(String search, int pag, int itens);
+
+    LibrasOutputDTO findByPalavra(String palavra);
 
     void delete(Long id);
 

17de8a6c0abfe4c79620a0b894b4230b6f562e01
Walmircsanto <walmirsantosrosa@gmail.com>
2024-12-30 21:04:39 -0300

    :sparkles: feat: Adicionar e implementar o metodo de busca profunda no controller

--- src/main/java/com/example/IfGoiano/IfCoders/controller/LibrasController.java
+++ src/main/java/com/example/IfGoiano/IfCoders/controller/LibrasController.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -45,55 +46,55 @@ public class LibrasController {
     @Operation(summary = "Buscar sinal por ID", tags = "Sinais de Libras")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Found the sign",
-                    content = { @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = InterpreteInputDTO.class)) }),
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = InterpreteInputDTO.class))}),
             @ApiResponse(responseCode = "404", description = "Sign not found",
                     content = @Content),
             @ApiResponse(responseCode = "500", description = "Internal server error",
                     content = @Content)
     })
     @GetMapping("find/{id}")
-    public ResponseEntity<LibrasOutputDTO> findById(@PathVariable Long id){
+    public ResponseEntity<LibrasOutputDTO> findById(@PathVariable Long id) {
         return new ResponseEntity<>(librasService.findById(id), HttpStatus.OK);
     }
 
     @Operation(summary = "Cadastrar um novo sinal", tags = "Sinais de Libras")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Sign created",
-                    content = { @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = LibrasOutputDTO.class)) }),
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = LibrasOutputDTO.class))}),
             @ApiResponse(responseCode = "500", description = "Internal server error",
-                    content = @Content) })
+                    content = @Content)})
     @PostMapping
     public ResponseEntity<LibrasOutputDTO> save(@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Dados do sinal a ser cadastrado", required = true,
-            content = @Content(schema = @Schema(implementation = LibrasInputDTO.class)))  @org.springframework.web.bind.annotation.RequestBody LibrasInputDTO sinais){
+            content = @Content(schema = @Schema(implementation = LibrasInputDTO.class))) @org.springframework.web.bind.annotation.RequestBody LibrasInputDTO sinais) {
         return new ResponseEntity<>(librasService.save(sinais), HttpStatus.CREATED);
     }
 
 
     @PostMapping("/sugere/{id}")
-    public ResponseEntity<LibrasOutputDTO> sugereLibras(@RequestBody LibrasInputDTO sinais,@PathVariable Long id){
+    public ResponseEntity<LibrasOutputDTO> sugereLibras(@RequestBody LibrasInputDTO sinais, @PathVariable Long id) {
 
-        return new ResponseEntity<>(librasService.sugereLibras(sinais,id), HttpStatus.CREATED);
+        return new ResponseEntity<>(librasService.sugereLibras(sinais, id), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<LibrasOutputDTO> getByIdLibras(@PathVariable Long id){
+    public ResponseEntity<LibrasOutputDTO> getByIdLibras(@PathVariable Long id) {
 
         return new ResponseEntity<>(librasService.findById(id), HttpStatus.OK);
     }
 
     @GetMapping()
-    public ResponseEntity<List<LibrasOutputDTO>> getAllLibras( @RequestParam int pag,
-                                                               @RequestParam int itens){
+    public ResponseEntity<List<LibrasOutputDTO>> getAllLibras(@RequestParam int pag,
+                                                              @RequestParam int itens) {
 
-        return new ResponseEntity<>(librasService.findAll(pag,itens), HttpStatus.OK);
+        return new ResponseEntity<>(librasService.findAll(pag, itens), HttpStatus.OK);
     }
 
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<LibrasOutputDTO> updateLibras(@PathVariable Long id,@RequestBody LibrasInputDTO sinais){
-        return new ResponseEntity<>(librasService.update(sinais,id), HttpStatus.NO_CONTENT);
+    public ResponseEntity<LibrasOutputDTO> updateLibras(@PathVariable Long id, @RequestBody LibrasInputDTO sinais) {
+        return new ResponseEntity<>(librasService.update(sinais, id), HttpStatus.NO_CONTENT);
     }
 
     @Operation(summary = "Deletar um sinal por ID", tags = "Sinais de Libras")
@@ -106,8 +107,17 @@ public class LibrasController {
                     content = @Content)
     })
     @DeleteMapping("/{id}")
-    public ResponseEntity<LibrasOutputDTO> delete(@PathVariable Long id){
+    public ResponseEntity<LibrasOutputDTO> delete(@PathVariable Long id) {
         librasService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+    @GetMapping("/busca-profunda")
+    public ResponseEntity<Page<LibrasOutputDTO>> buscaProfundaLibras
+            (String palavra, @RequestParam(defaultValue = "0") int pag, @RequestParam(defaultValue = "10") int itens) {
+        Page<LibrasOutputDTO> resultados = this.librasService.searchLibrasByDeeply(palavra, pag, itens);
+        return ResponseEntity.ok(resultados);
+    }
+
+
 }

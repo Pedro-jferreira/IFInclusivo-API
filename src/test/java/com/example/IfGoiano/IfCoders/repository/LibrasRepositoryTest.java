11378aa4a597eed209e4a3ab4ee238d93e820dbf
Walmircsanto <walmirsantosrosa@gmail.com>
2025-01-15 08:57:26 -0300

    :white_check_mark: test: adicionado teste de busca rasa de libras

--- src/test/java/com/example/IfGoiano/IfCoders/repository/LibrasRepositoryTest.java
+++ src/test/java/com/example/IfGoiano/IfCoders/repository/LibrasRepositoryTest.java
@@ -1,11 +1,89 @@
 package com.example.IfGoiano.IfCoders.repository;
 
-import org.junit.jupiter.api.Test;
-
+import com.example.IfGoiano.IfCoders.controller.DTO.input.InterpreteInputDTO;
+import com.example.IfGoiano.IfCoders.entity.ConfigAcessibilidadeEntity;
+import com.example.IfGoiano.IfCoders.entity.Enums.Categorias;
+import com.example.IfGoiano.IfCoders.entity.Enums.Status;
 import static org.junit.jupiter.api.Assertions.*;
+import com.example.IfGoiano.IfCoders.entity.InterpreteEntity;
+import com.example.IfGoiano.IfCoders.entity.LibrasEntity;
+import com.example.IfGoiano.IfCoders.mapper.mocks.MockConfigAcess;
+import com.example.IfGoiano.IfCoders.mapper.mocks.MockInterprete;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest // indica que e uma classe de teste que vai testa o repositoryJPA
+@ActiveProfiles("test") //indica qual arquivo de properties vai ser usado para os teste
 class LibrasRepositoryTest {
 
+    MockConfigAcess input = new MockConfigAcess();
+
+    MockInterprete inputInterprete = new MockInterprete();
+
+    @Autowired
+    LibrasRepository librasRepository;
+
+    @Autowired
+    EntityManager entityManager;
+
+    private InterpreteEntity createInterprete(InterpreteInputDTO user) {
+        InterpreteEntity interpreteEntity = new InterpreteEntity();
+        interpreteEntity.setNome(user.getNome());
+        interpreteEntity.setSalary(user.getSalary());
+        interpreteEntity.setLogin(user.getLogin());
+        interpreteEntity.setMatricula(user.getMatricula());
+        interpreteEntity.setBiografia(user.getBiografia());
+        interpreteEntity.setEspecialidade(user.getEspecialidade());
+        interpreteEntity.setSenha(user.getSenha());
+        ConfigAcessibilidadeEntity config = entityManager.merge(input.mockConfigAces(1L));
+        this.entityManager.persist(interpreteEntity);// Persistir primeiro
+        interpreteEntity.setConfigAcessibilidadeEntity(config); // Associar após persistência
+        this.entityManager.persist(interpreteEntity); // Persistir InterpreteEntity
+
+        return interpreteEntity;
+    }
+
+//    private ConfigAcessibilidadeEntity createConfigAcessibilidadeEntity(ConfigAcblInputDTO config) {
+//        ConfigAcessibilidadeEntity configAcessibilidadeEntity = new ConfigAcessibilidadeEntity();
+//        config.setAudicao(config.getAudicao());
+//        config.setTema(config.getTema());
+//        config.setZoom(config.getZoom());
+//        this.entityManager.persist(config);
+//        return configAcessibilidadeEntity;
+//    }
+
+
     @Test
-    void searchLibrasByDeeply() {
+    @DisplayName("should create Libras successfully from DB")
+    void searchLibrasByDeeplySuccess() {
+        LibrasEntity librasEntity = new LibrasEntity();
+        librasEntity.setId(1L);
+        librasEntity.setStatus(Status.APROVADO);
+        librasEntity.setFoto("First foto" + 1L);
+        librasEntity.setDescricao("First descricao" + 1L);
+        librasEntity.setPalavra("First palavra" + 1L);
+        librasEntity.setUrl("First url" + 1L);
+        librasEntity.setJustificativa("First justificativa" + 1L);
+        librasEntity.setVideo("First video" + 1L);
+        librasEntity.setCategorias(Categorias.REDES);
+        InterpreteInputDTO interpreteInputDTO =  this.inputInterprete.interpreteInputDTO(1L);
+        librasEntity.getSugeriu().add(this.createInterprete(interpreteInputDTO));
+
+        this.entityManager.merge(librasEntity);
+
+        Page<LibrasEntity> result =  this.librasRepository.searchLibrasByDeeply("First", PageRequest.of(0, 1) );
+        assertThat(result.getTotalElements()).isEqualTo(1);
+        assertNotNull(result.getContent());
+        assertEquals("First foto1", result.getContent().get(0).getFoto());
+
+
     }
 }

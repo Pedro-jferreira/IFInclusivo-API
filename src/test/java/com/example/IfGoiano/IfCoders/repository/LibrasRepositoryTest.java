d8b99b0eb1db0b874af192febcdaa305d9a13392
Walmircsanto <walmirsantosrosa@gmail.com>
2025-01-15 08:55:02 -0300

    :white_check_mark: test: criado classe de testes libras

---
+++ src/test/java/com/example/IfGoiano/IfCoders/repository/LibrasRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.example.IfGoiano.IfCoders.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LibrasRepositoryTest {
+
+    @Test
+    void searchLibrasByDeeply() {
+    }
+}

4f500102f545e409c16917253882a71bd6087cc1
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-01-23 09:10:27 -0300

    :recycle: refactor (topicoMapper) correcoes de mapeamento
    adicao de parse manual

--- src/main/java/com/example/IfGoiano/IfCoders/controller/mapper/TopicoMapper.java
+++ src/main/java/com/example/IfGoiano/IfCoders/controller/mapper/TopicoMapper.java
@@ -6,6 +6,7 @@ import com.example.IfGoiano.IfCoders.controller.DTO.SimpleTopicoDTO;
 import com.example.IfGoiano.IfCoders.controller.DTO.input.TopicoInputDTO;
 import com.example.IfGoiano.IfCoders.controller.DTO.output.TopicoOutputDTO;
 import com.example.IfGoiano.IfCoders.entity.Enums.Categorias;
+import com.example.IfGoiano.IfCoders.entity.PublicacaoEntity;
 import com.example.IfGoiano.IfCoders.entity.TopicoEntity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.modelmapper.ModelMapper;
@@ -23,6 +24,9 @@ public class TopicoMapper {
     @Lazy
     @Autowired
     ProfessorMapper professorMapper;
+@Lazy
+    @Autowired
+    PublicacaoMapper publicacaoMapper;
 
     public SimpleTopicoDTO toSimpleTopicoDTO(TopicoEntity topicoEntity) {
         SimpleTopicoDTO simpleTopicoDTO = new SimpleTopicoDTO();
@@ -37,15 +41,6 @@ public class TopicoMapper {
     }
 
 
-
-
-    private Long id;
-    private String titulo;
-    private String descricao;
-    private Categorias categoria;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime dataCriacao;
-    private SimpleProfessorDTO professor;
     public TopicoEntity toTopicoEntity(SimpleTopicoDTO simpleTopicoDTO) {
         return modelMapper.map(simpleTopicoDTO, TopicoEntity.class);
     }
@@ -58,7 +53,20 @@ public class TopicoMapper {
     }
 
     public TopicoOutputDTO toTopicoOutputDTO(TopicoEntity topicoEntity) {
-        return modelMapper.map(topicoEntity, TopicoOutputDTO.class);
+        TopicoOutputDTO output = new TopicoOutputDTO();
+        output.setId(topicoEntity.getId());
+        output.setTitulo(topicoEntity.getTitulo());
+        output.setDescricao(topicoEntity.getDescricao());
+        output.setCategoria(topicoEntity.getCategoria());
+        output.setDataCriacao(topicoEntity.getDataCriacao());
+        output.setProfessor(professorMapper.toSimpleProfessorDTO(topicoEntity.getProfessor()));
+        if (!topicoEntity.getPublicacaoEntities().isEmpty()){
+            for (PublicacaoEntity publicacaoEntity : topicoEntity.getPublicacaoEntities()) {
+                output.getPublicacoes().add(publicacaoMapper.toSimplePublicacaoDTO(publicacaoEntity));
+            }
+        }
+
+        return output;
     }
     public TopicoEntity toTopicoEntity(TopicoOutputDTO inputDTO) {
         return modelMapper.map(inputDTO, TopicoEntity.class);

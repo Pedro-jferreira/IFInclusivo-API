304ad0f2be4b22781fcde2ebfafe136bdaf61a7c
walmir.rosa <walmirsantosrosa@gmail.com>
2025-09-15 16:53:14 -0300

    :sparkles: feat: Implementado Exeções personalizadas

--- src/main/java/com/example/IfGoiano/IfCoders/exception/TokenExpiredException.java
+++ src/main/java/com/example/IfGoiano/IfCoders/exception/TokenExpiredException.java
@@ -1,4 +1,8 @@
 package com.example.IfGoiano.IfCoders.exception;
 
-public class TokenExpiredException {
+
+public class TokenExpiredException extends RuntimeException {
+    public TokenExpiredException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/example/IfGoiano/IfCoders/exception/TokenInvalidException.java
+++ src/main/java/com/example/IfGoiano/IfCoders/exception/TokenInvalidException.java
@@ -1,4 +1,8 @@
 package com.example.IfGoiano.IfCoders.exception;
 
-public class TokenInvalidException {
+public class TokenInvalidException extends RuntimeException{
+
+public TokenInvalidException(String message) {
+        super(message);
+    }
 }

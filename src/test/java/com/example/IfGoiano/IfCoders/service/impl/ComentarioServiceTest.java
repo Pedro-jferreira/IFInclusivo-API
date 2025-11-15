e918f5f4011c9fbf954d96f08be92554e980303a
Isaias Valdemir <112592786+Isaiasiv@users.noreply.github.com>
2025-11-14 22:09:31 -0300

    test(ComentarioService): Corrige typo no teste de editarComentario

--- src/test/java/com/example/IfGoiano/IfCoders/service/impl/ComentarioServiceTest.java
+++ src/test/java/com/example/IfGoiano/IfCoders/service/impl/ComentarioServiceTest.java
@@ -9,6 +9,8 @@ import com.example.IfGoiano.IfCoders.entity.UsuarioEntity;
 import com.example.IfGoiano.IfCoders.repository.ComentarioRepository;
 import com.example.IfGoiano.IfCoders.repository.PublicacaoRepositoy;
 import com.example.IfGoiano.IfCoders.repository.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.hibernate.action.internal.EntityActionVetoException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -106,11 +108,79 @@ class ComentarioServiceTest {
     }
 
     @Test
-    void editarComentario() {
+    @DisplayName("Deve lançar exceção ao adicionar comentário se usuário não existir")
+    void adicionarComentario_DeveLancarExcecao_QuandoUsuarioNaoEncontrado() {
+
+        when(usuarioRepository.findByLogin("fantasma")).thenReturn(Optional.empty());
+        assertThrows(EntityNotFoundException.class, () -> {
+            comentarioService.adicionarComentario(10L, mockRequestDTO, "fantasma");
+        });
+        verify(comentarioRepository, never()).save(any());
     }
 
     @Test
-    void excluirComentario() {
+    @DisplayName("Deve excluir um comentário com sucesso")
+    void excluirComentario_DeveExcluirComSucesso_QuandoForAutor() {
+
+        mockComentarioEntity.setUsuario(mockUsuario);
+        when(comentarioRepository.findById(100L)).thenReturn(Optional.of(mockComentarioEntity));
+        comentarioService.excluirComentario(100L, "Isaias");
+        verify(comentarioRepository, times(1)).delete(mockComentarioEntity);
+
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção de segurança ao tentar excluir comentário de outro usuário")
+    void excluirComentario_DeveLancarExcecao_QuandoNaoForAutor() {
+
+        mockComentarioEntity.setUsuario(mockUsuario);
+        when(comentarioRepository.findById(100L)).thenReturn(Optional.of(mockComentarioEntity));
+        assertThrows(SecurityException.class, () -> {
+            comentarioService.excluirComentario(100L, "maria");
+        });
+        verify(comentarioRepository, never()).delete(any());
+    }
+
+    @Test
+    @DisplayName("Deve ediatr um comentario com sucesso")
+    void editarComentario_DeveEditarComSucesso() {
+
+        Long comentarioId = 100L;
+        String usernameDono = "Isaias";
+
+        //novo comentario
+        ComentarioRequestDTO updateRequest = new ComentarioRequestDTO();
+        updateRequest.setTexto("Texto Atualizado");
+
+        //antigo comentario
+        mockComentarioEntity.setId(comentarioId);
+        mockComentarioEntity.setTexto("Texto antigo");
+        mockComentarioEntity.setUsuario(mockUsuario);
+
+        //comportamento
+        when(comentarioRepository.findById(comentarioId)).thenReturn(Optional.of(mockComentarioEntity));//retone comentario existente
+        when(comentarioRepository.save(any(ComentarioEntity.class))).thenAnswer(invocation -> invocation.getArgument(0));//atualiza comentario
+        ComentarioResponseDTO responseDTOAtualizado = new ComentarioResponseDTO();
+        responseDTOAtualizado.setId(comentarioId);
+        responseDTOAtualizado.setTexto("Texto Atualizado");
+
+        when(comentarioMapper.toResponseDTO(any(ComentarioEntity.class), eq(mockUsuario))).thenReturn(responseDTOAtualizado);
+
+        ComentarioResponseDTO result = comentarioService.editarComentario(comentarioId, updateRequest, usernameDono);
+
+        assertNotNull(result);
+        assertEquals(comentarioId, result.getId());
+        assertEquals("Texto Atualizado", result.getTexto());
+
+        assertEquals("Texto Atualizado", mockComentarioEntity.getTexto());
+
+        verify(comentarioRepository, times(1)).findById(comentarioId); // Verificamos se ele procurou o comentário
+        verify(comentarioRepository, times(1)).save(mockComentarioEntity); // Verificamos se ele salvou o comentário
+        verify(comentarioMapper, times(1)).toResponseDTO(mockComentarioEntity, mockUsuario); // Verificamos se ele mapeou a resposta
+
+        verify(usuarioRepository, never()).findByLogin(anyString());
+        verify(publicacaoRepository, never()).findById(anyLong());
+
     }
 
     @Test

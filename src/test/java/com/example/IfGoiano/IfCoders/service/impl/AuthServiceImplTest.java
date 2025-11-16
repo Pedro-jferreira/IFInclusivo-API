d0c551bfa8789679f94ea4adef7cdb3387ac67fd
Isaias Valdemir <112592786+Isaiasiv@users.noreply.github.com>
2025-11-15 21:40:01 -0300

    test(AuthServiceImpl): adiciona testes de registro de usuário
    Cria a suíte de testes para o AuthServiceImpl.
    
    Adiciona os testes iniciais cobrindo os cenários do método 'register'

---
+++ src/test/java/com/example/IfGoiano/IfCoders/service/impl/AuthServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.example.IfGoiano.IfCoders.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceImplTest {
+
+    @Test
+    void authenticate() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void verificationToken() {
+    }
+
+    @Test
+    void forgotPassword() {
+    }
+
+    @Test
+    void resetPassword() {
+    }
+
+    @Test
+    void updatePassword() {
+    }
+
+    @Test
+    void resendConfirmationEmailFromExpiredToken() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void logout() {
+    }
+}
